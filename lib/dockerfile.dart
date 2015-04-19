92245562e521c352b0d603386e099a926236c8be
Anders Holmgren <andersmholmgren@gmail.com>
2015-04-20 08:01:44 +1000

    first

---
+++ lib/dockerfile.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2015, Anders Holmgren. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The dockerfile library.
+///
+/// A library for manipulating [Dockerfile](https://docs.docker.com/reference/builder/)
+library dockerfile;
+
+export 'src/dockerfile.dart';
